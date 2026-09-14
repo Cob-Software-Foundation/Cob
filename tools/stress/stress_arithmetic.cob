4e9d7d32cb9abe03ab8cf23fbb007062c646668f
m97493578-ops <m97493578@gmail.com>
2026-09-14 19:20:48 -0400

    added v0.0.5 code --geting ready for merge

--- tools/stress/stress_arithmetic.cob
+++
@@ -1,7 +0,0 @@
-_MakeCache = False
-set i = 0
-set total = 0
-while i < 200000:
-    set total = total + i
-    set i = i + 1
-pop("arithmetic stress: total=" + total + " iterations=" + i)
--- tools/stress/widget_visual.cob
+++
@@ -1,9 +0,0 @@
-_MakeCache = False
-shuck cobwindow
-set w = window_open("Widget Visual Check")
-set rc = window_label(w, "Hello from Cob widgets!")
-set rc = window_button(w, "Click me")
-set rc = window_slider(w, "Volume", 100)
-set rc = window_textbox(w, "Name")
-set rc = window_wait(w, 3)
-set rc = window_close(w)
